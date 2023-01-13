5fc81929464a6d8a7de6973968aab259b25a5c56
Ed Garcia <edgarcia@Eds-MacBook-Air.local>
2023-01-12 19:19:07 -0600

    use nav bar component in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,4 @@
 <template>
-  <div>
-    <nav class="navbar">
-      <p>
-        <a class="link" href="#">
-          Explore the fret
-        </a>
-      </p>
-      <p>
-        <a class="link" href="https://github.com/beeetooo/guitar-scales" target="_blank"> Github </a>
-      </p>
-    </nav>
-    <Nuxt />
-  </div>
+  <NavBar />
 </template>
 
-<style>
-html {
-  width: 100%;
-  height: 100%;
-  font-family: Poppins;
-}
-
-body {
-  margin: 0;
-}
-
-.navbar {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  padding: 0 24px 0 24px;
-  background-color: #7692ff;
-}
-
-.link {
-  text-decoration: none;
-  color: black;
-}
-</style>
